61d3138c7b3b4a352bcf94c60bc26004abd1a0f1
23005659 <163414615+23005659@users.noreply.github.com>
2025-03-06 22:44:07 +0200

    Add files via upload

---
+++ areatriangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class area{
+    public static void main(String[] args) {
+        
+        double base = 10;
+        double height = 45;
+        double area;
+
+        area = 0.5*base*height;
+
+        System.out.println("The area of the triangle is: " + area);
+    }
+}
